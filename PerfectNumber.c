ce4dc58a1ebd81e1d14f19f8f61101c2e06c11b5
Gökhan TOPUZ <gokhan061@msn.com>
2023-04-01 18:48:01 +0300

    Create PerfectNumber.c

---
+++ PerfectNumber.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void perfectNumber(int num);
+
+int main()
+{
+    int num;
+    printf("Please enter a integer: ");
+    scanf("%d",&num);
+    perfectNumber(num);
+
+    return 0;
+}
+
+void perfectNumber(int num)
+{
+    int result = 0;
+
+    for(int i = 1; i < num; i++)
+    {
+        if(num % i == 0)
+        {
+            result += i;
+        }
+        else
+            continue;
+    }
+    if(num == result)
+        printf("%d is Perfect Number.",num);
+    else
+        printf("%d is not Perfect Number.",num);
+}
